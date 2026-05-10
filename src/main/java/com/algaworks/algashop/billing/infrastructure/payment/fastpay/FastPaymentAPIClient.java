57e07df2ec501ebae82529f4a0ed12b848d76da7
CAIO <caiohgs13@gmail.com>
2026-05-10 13:01:25 -0300

    finalização de cancelamento de faturas com processamento em lote (Batch) e implementação da API fastpay

--- src/main/java/com/algaworks/algashop/billing/infrastructure/payment/fastpay/FastPaymentAPIClient.java
+++ src/main/java/com/algaworks/algashop/billing/infrastructure/payment/fastpay/FastPaymentAPIClient.java
@@ -1,7 +1,6 @@
 package com.algaworks.algashop.billing.infrastructure.payment.fastpay;
 
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
